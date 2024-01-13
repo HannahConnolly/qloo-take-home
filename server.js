65cbde201420c0e4b11c7e4d1b09216b2912d60a
HannahConnolly <connollyhannah93@gmail.com>
2024-01-13 17:47:12 -0500

    prettier format

--- server.js
+++ server.js
@@ -1,74 +1,81 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 
 const app = express();
 const port = 3001;
 
 app.use(express.json());
 
-const DEBUG = true
-const GEN_1_POKEMON = 151
+const DEBUG = true;
+const GEN_1_POKEMON = 151;
 
 // "database" of current pokemon
-const pokemonTeam = []
+const pokemonTeam = [];
 
-app.get('/fetch-data/:pokemonID?', async (req, res) => {
+app.get("/fetch-data/:pokemonID?", async (req, res) => {
   try {
-    let pokemonID = Number.parseInt(req.params.pokemonID)
+    const pokemonID = Number.parseInt(req.params.pokemonID);
 
     // handle bad param input
-    if(pokemonID === NaN){ 
-      throw Error("Pokemon ID provided could not be parsed as a number")
+    if (pokemonID === NaN) {
+      throw Error("Pokemon ID provided could not be parsed as a number");
     }
 
     // no pokemon ID provided, call random pokemon
-    if(!pokemonID){
-      pokemonID = Math.ceil(Math.random() * GEN_1_POKEMON)
+    if (!pokemonID) {
+      pokemonID = Math.ceil(Math.random() * GEN_1_POKEMON);
     }
-    
+
     // Contact the Pokemon API to fetch data
-    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`);
+    const response = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonID}`
+    );
     const data = response.data;
 
-    if(DEBUG){ console.log(`200 : /fetch-data recieved a ${data.name}`) }
+    if (DEBUG) {
+      console.log(`200 : /fetch-data recieved a ${data.name}`);
+    }
 
     res.json(data);
   } catch (error) {
-    console.error('Error fetching data from Pokemon API:', error.message);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error fetching data from Pokemon API:", error.message);
+    res.status(500).json({ error: "Error Reaching PokemonAPI" });
   }
 });
 
 // add pokemon to team
-app.post('/process-data', (req, res) => {
+app.post("/process-data", (req, res) => {
   try {
     // Process the data received in the request body
-    const {id, name, types} = req.body;
+    const { id, name, types } = req.body;
 
-    let typeNames = types.map(type => type.type.name)
+    let typeNames = types.map((type) => type.type.name);
 
     const parsedPokemon = {
       name,
       id,
-      types: typeNames
-    }
+      types: typeNames,
+    };
 
     // a team can only have 6 pokemon - remove oldest
-    if(pokemonTeam.length > 5) {
-      pokemonTeam.shift()
+    if (pokemonTeam.length > 5) {
+      pokemonTeam.shift();
     }
 
-    pokemonTeam.push(parsedPokemon)
+    pokemonTeam.push(parsedPokemon);
 
-    if(DEBUG){ 
-      console.log(`200 : /process-data received a ${name}`)
-      console.log(pokemonTeam) 
+    if (DEBUG) {
+      console.log(`200 : /process-data received a ${name}`);
+      console.log(pokemonTeam);
     }
 
-    res.json({ message: 'Pokemon processed successfully', team: pokemonTeam });
+    res.json({ message: "Pokemon processed successfully", team: pokemonTeam });
   } catch (error) {
-    console.error(`500 : /process-data - Error processing data: `, error.message);
-    res.status(500).json({ error: 'Error processing input Pokemon' });
+    console.error(
+      `500 : /process-data - Error processing data: `,
+      error.message
+    );
+    res.status(500).json({ error: "Error processing input Pokemon" });
   }
 });
 
